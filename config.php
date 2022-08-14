ea469f422d7f0b6fb5f84af47eed9e6463607984
ufuomaolori <23246216+ufuomaolori@users.noreply.github.com>
2022-08-14 18:42:23 +0100

    Add files via upload
    Web application files added

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'marotech.mysql.database.azure.com');
+define('DB_USERNAME', 'admin1');
+define('DB_PASSWORD', 'Pa$$w0rd1234');
+define('DB_NAME', 'users');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+
+?>
